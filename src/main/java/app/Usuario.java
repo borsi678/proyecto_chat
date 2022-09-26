523a7a54ce49426f94ee9c46a6a661a878f06113
angel <borsi678@hotmail.es>
2022-09-26 01:47:25 -0500

    Clase Usuario completada

--- src/main/java/app/Usuario.java
+++ src/main/java/app/Usuario.java
@@ -2,4 +2,42 @@ package app;
 
 public class Usuario {
     
+    private String nombre;
+    private EstadoUsuario estado;
+    
+    public Usuario(){
+        this.nombre="";
+        this.estado=EstadoUsuario.ACTIVE;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+    
+    public EstadoUsuario getEstado(){
+        return estado;
+    }
+    
+    public void setEstado(EstadoUsuario estado){
+        this.estado=estado;
+    }
+    
+    @Override public String toString(){
+        return "Nombre: "+nombre+" Estado: "+estado.toString();
+    }
+    
+    @Override public boolean equals(Object objeto){
+        if(objeto == null)
+            return false;
+        if(!(objeto instanceof Usuario))
+            return false;
+        Usuario usuarioComparar=(Usuario) objeto;
+        if(this.nombre.equals(usuarioComparar.nombre) && this.estado==usuarioComparar.estado)
+            return true;
+        return false;
+    }
 }
