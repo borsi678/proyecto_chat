99b13b7b629c94e5f8f389b26e02637e6a66ccb0
angel <borsi678@hotmail.es>
2022-09-27 01:53:49 -0500

    Clase ConstructorMensajes

--- src/main/java/app/ConstructorMensajes.java
+++ src/main/java/app/ConstructorMensajes.java
@@ -1,5 +1,40 @@
 package app;
 
 public class ConstructorMensajes {
+    Mensajes mensaje;
     
+    public ConstructorMensajes(){
+        this.mensaje=new Mensajes();
+    }
+    
+    public ConstructorMensajes conTipo(String tipo){
+        this.mensaje.setTipo(tipo);
+        return this;
+    }
+    
+    public ConstructorMensajes conMensaje(String mensaje){
+        this.mensaje.setMensaje(mensaje);
+        return this;
+    }
+
+    public ConstructorMensajes conOperacion(String operacion){
+        this.mensaje.setOperacion(operacion);
+        return this;
+    }
+
+    public ConstructorMensajes conNombreUsuario(String nombreUsuario){
+        this.mensaje.setNombreUsuario(nombreUsuario);
+        return this;
+    }
+    
+    public ConstructorMensajes conNosmbresUsuarios(String[] nombresUsuarios){
+        this.mensaje.setNombresUsuarios(nombresUsuarios);
+        return this;
+    }
+
+    public Mensajes construyeMensaje() throws Exception {
+        if(!this.mensaje.estadoValido())
+            throw new Exception("Estado de mensaje invalido");
+        return this.mensaje;
+    }
 }
--- src/main/java/app/Cuarto.java
+++ src/main/java/app/Cuarto.java
@@ -1,5 +1,116 @@
 package app;
 
+import java.util.LinkedList;
+
 public class Cuarto {
+    private String nombre;
+    private LinkedList<Usuario> usuariosUnidos;
+    private LinkedList<Usuario> usuariosInvitados;
+    private Usuario usuarioCreador;
     
+    public Cuarto(){
+        this.nombre ="";
+        this.usuarioCreador = null;        
+        this.usuariosUnidos = new LinkedList<Usuario>();
+        this.usuariosInvitados = new LinkedList<Usuario>();
+    }
+
+    public Cuarto(String nombre, Usuario usuarioCreador) {
+        this.nombre = nombre;
+        this.usuarioCreador = usuarioCreador;        
+        this.usuariosUnidos = new LinkedList<Usuario>();
+        this.usuariosInvitados = new LinkedList<Usuario>();
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LinkedList<Usuario> getUsuariosUnidos() {
+        return usuariosUnidos;
+    }
+
+    public void setUsuariosUnidos(LinkedList<Usuario> usuariosUnidos) {
+        this.usuariosUnidos = usuariosUnidos;
+    }
+
+    public LinkedList<Usuario> getUsuariosInvitados() {
+        return usuariosInvitados;
+    }
+
+    public void setUsuariosInvitados(LinkedList<Usuario> usuariosInvitados) {
+        this.usuariosInvitados = usuariosInvitados;
+    }
+
+    public Usuario getUsuarioCreador() {
+        return usuarioCreador;
+    }
+
+    public void setUsuarioCreador(Usuario usuarioCreador) {
+        this.usuarioCreador = usuarioCreador;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Cuarto)) {
+            return false;
+        }
+        final Cuarto cuartoComparar = (Cuarto) obj;
+        if (!(this.nombre.equals(cuartoComparar.nombre))) {
+            return false;
+        }
+        if (!(this.usuarioCreador.equals(cuartoComparar.usuarioCreador))) {
+            return false;
+        }
+        if (!(this.usuariosUnidos.equals(cuartoComparar.usuariosUnidos))) {
+            return false;
+        }       
+        return this.usuariosInvitados.equals(cuartoComparar.usuariosInvitados);
+    }
+    
+    public boolean estaUsuarioUnido(Usuario usuarioEncontrar){
+        return usuariosUnidos.contains(usuarioEncontrar);
+    }
+    
+    public boolean estaUsuarioInvitado(Usuario usuarioEncontrar){
+        return usuariosInvitados.contains(usuarioEncontrar);
+    }
+    
+    public void agregaUsuarioInvitado(Usuario invitado){
+        this.usuariosInvitados.add(invitado);
+    }
+    
+    public void agregaUsuarioUnido(Usuario invitado){
+        this.usuariosUnidos.add(invitado);
+    }
+    
+    public void eliminaUsuarioUnido(Usuario usuarioEliminado){
+        this.usuariosUnidos.remove(usuarioEliminado);
+    }
+    
+    public void eliminaUsuarioInvitado(Usuario usuarioEliminado){
+        this.usuariosInvitados.remove(usuarioEliminado);
+    }
+    
+    public  boolean esVacia(){
+        return usuariosUnidos.isEmpty();
+    }
+    
+    public int cuantosUsuariosHayUnidos(){
+        return usuariosUnidos.size();
+    }
+    
+    public int cuantosUsuariosHayInvitados(){
+        return usuariosInvitados.size();
+    }
 }
