c09fc37cc116ed524f289f6160854cc81c222e5c
angel <borsi678@hotmail.es>
2022-09-28 02:04:59 -0500

    Probando JSON

--- src/main/java/app/Procesador.java
+++ src/main/java/app/Procesador.java
@@ -1,5 +1,14 @@
 package app;
 
+import com.alibaba.fastjson.JSON;
+
 public abstract class Procesador {
     
+    public String serializaMensaje(Mensajes mensaje){
+        return JSON.toJSONString(mensaje);
+    }
+    
+    public Mensajes deserializaMensaje(String mensaje){
+        return JSON.parseObject(mensaje, Mensajes.class );
+    }
 }
--- src/main/java/app/TiposMensaje.java
+++ src/main/java/app/TiposMensaje.java
@@ -8,4 +8,37 @@ public  enum TiposMensaje {
     
     ERROR, WARNING,  INFO, INVALID;
     
+    @Override public String toString(){
+        switch(this){
+            case ERROR: return "ERROR";
+            case WARNING: return "WARNING";
+            case INFO: return "INFO";
+            case IDENTIFY: return "IDENTIFY";
+            case NEW_USER: return "NEW_USER";
+            case STATUS: return "STATUS";
+            case NEW_STATUS: return "NEW_STATUS";
+            case USERS: return "USERS";
+            case USER_LIST: return "USER_LIST";
+            case MESSAGE: return "MESSAGE";
+            case MESSAGE_FROM: return "MESSAGE_FROM";
+            case PUBLIC_MESSAGE: return "PUBLIC_MESSAGE";
+            case PUBLIC_MESSAGE_FROM: return "PUBLIC_MESSAGE_FROM";
+            case NEW_ROOM: return "NEW_ROOM";
+            case INVITE: return "INVITE";
+            case INVITATION: return "INVITATION";
+            case JOIN_ROOM: return "JOIN_ROOM";
+            case JOINED_ROOM: return "JOINED_ROOM";
+            case ROOM_USERS: return "ROOM_USERS";
+            case ROOM_USER_LIST: return "ROOM_USER_LIST";
+            case ROOM_MESSAGE: return "ROOM_MESSAGE";
+            case ROOM_MESSAGE_FROM:  return "ROOM_MESSAGE_FROM";            
+            case LEAVE_ROOM: return "LEAVE_ROOM";
+            case LEFT_ROOM: return "LEFT_ROOM";
+            case DISCONNECTED: return "DISCONNECTED";
+            case DISCONNECT: return "DISCONNECT";
+            case INVALID: return "INVALID";
+            default: return "";
+            
+        }
+    }
 }
